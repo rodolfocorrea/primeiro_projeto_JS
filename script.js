df8e4ebe3a661fcf491c579d5eb68b8fa4034d77
Rodolfo <correa.rodolfo@outlook.com>
2021-07-31 00:03:16 -0300

    primeiro projeto com JS

---
+++ script.js
@@ -0,0 +1,9 @@
+
+//Alerta na tela 
+alert("Bem vindo ao meu primeiro script")
+
+//Alerta de confirmação, sempre uma pergunta.
+confirm("Vamos com tudo nos estudos?")
+
+//Alerta para comentários em geral.
+prompt("Escreva uma mensagem motivacional:")
